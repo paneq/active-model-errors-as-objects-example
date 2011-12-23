a5f894b7c3e2e548b24db9bc50e147e72fdc57d9
Robert Pankowecki <robert.pankowecki@gmail.com>
2011-12-23 16:49:29 +0100

    Implementation that leads to I18n::InvalidPluralizationData

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  def error_message(error)
+    return error if error.is_a?(String)
+    method = error[:code].downcase + "_error_message"
+    return send(method) if respond_to?(method) # We have a special way of displaying such error
+    return error[:description]                 # Just display the description from model
+  end
+
+  def invalid_length
+    "length of this text is invalid. Minimum is <strong>#{error[:min]}</strong> and maximum is <strong>#{error[:max]}</strong>".html_safe
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  validates :login, :length => { :in => 2..10 }
+  validates :login,
+    length: {
+      in: 2..10,
+      message: {
+        code: :INVALID_LENGTH,
+        max: 10,
+        min: 2,
+        description: "Invalid length"
+    }
+  }
 end
